49a23cf08fa738bb44a53f460219cdbbcdf61995
Hoang Long <hoanglong.cmb@gmail.com>
2024-08-27 10:22:22 +0700

    update Answer

--- Testify.DAL/Models/Answer.cs
+++ Testify.DAL/Models/Answer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,9 @@ namespace Testify.DAL.Models
     {
         [Key]
         public int Id { get; set; }
+        [ForeignKey("QuestionId")]
+        public int QuestionId { get; set; }
+        public virtual Question? Question { get; set; }
         public string Content { get; set; }
         public bool IsCorrect { get; set; }
         public bool? Status { get; set; }
