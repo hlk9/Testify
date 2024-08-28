a653355898e1cc83264f68f85fa35d8c089a300e
Bacdtx <bacdtx.atwork@gmail.com>
2024-08-28 23:54:43 +0700

    fix them moi dap an va cau hoi, layout cua cauhoi va dap an

---
+++ Testify.Web/utils/OpenDialogType.cs
@@ -0,0 +1,12 @@
+﻿namespace Testify.Web.utils
+{
+    public static class OpenDialogType
+    {
+        public static string Create = "Create";
+        public static string Updatre = "Updatre";
+        public static string Detail = "Detail";
+        public static string Delete = "Delete";
+        public static string ShowAnswer = "ShowAnswer";
+        public static string ShowDialogAddNewAnswer = "ShowDialogAddNewAnswer";
+    }
+}
