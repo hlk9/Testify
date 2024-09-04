6e2c7d60d75d7789a6b425f177a650bbcb26bdfd
SNAKE01112004 <nuyendoanquangluc1112004@gmail.com>
2024-09-05 03:53:12 +0700

    commit_1

---
+++ Testify.API/Controllers/UserPermissionController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Testify.DAL.Models;
+using Testify.DAL.Reposiroties;
+
+namespace Testify.API.Controllers
+{
+    [Route("UserPermission")]
+    [ApiController]
+    public class UserPermissionController : ControllerBase
+    {
+        UserPermissionRepository _repUserPermission;
+        public UserPermissionController()
+        {
+            _repUserPermission = new UserPermissionRepository();
+        }
+        [HttpGet("Get-All-UserPermission")]
+        public async Task<ActionResult<List<UserPermission>>> GetAllUserPermission()
+        {
+            var lstUserPermission = await _repUserPermission.GetAllUserPermission();
+            return Ok(lstUserPermission);
+        }
+        [HttpGet("Get-By-Id-UserPermission")]
+        public async Task<ActionResult<UserPermission>> GetByIdUserPermission(int id)
+        {
+            var objUserPermission = await _repUserPermission.GetUserPermissionId(id);
+            return Ok(objUserPermission);
+        }
+        [HttpPost("Create-UserPermission")]
+        public async Task<ActionResult<UserPermission>> Create(UserPermission userPermission)
+        {
+            var createUserPermission = await _repUserPermission.CreateUserPermission(userPermission);
+            return Ok(createUserPermission);
+        }
+        [HttpPut("Update-UserPermission")]
+        public async Task<ActionResult<UserPermission>> Update(UserPermission userPermission)
+        {
+            var updateUserPermission = await _repUserPermission.UpdateUserPermission(userPermission);
+            return Ok(updateUserPermission);
+        }
+        [HttpDelete("Delete-UserPermission")]
+        public async Task<ActionResult<UserPermission>> Delete(int id)
+        {
+            var deleteUserPermission = await _repUserPermission.DeleteUserPermission(id);
+            return Ok(deleteUserPermission);
+        }
+    }
+}
---
+++ Testify.Web/Services/UserService.cs
@@ -0,0 +1,51 @@
+﻿using Testify.DAL.Models;
+
+namespace Testify.Web.Services
+{
+    public class UserService
+    {
+        private readonly HttpClient _httpClient;
+        public UserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<User>> GetAll()
+        {
+            return await _httpClient.GetFromJsonAsync<List<User>>("User/Get-All-User");
+        }
+        public async Task<User> GetByIdUser(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<User>($"User/Get-By-Id-User?id={id}");
+        }
+
+        public async Task<bool> Create(User user)
+        {
+            var result = await _httpClient.PostAsJsonAsync<User>("User/Create-User", user);
+            if (result.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<bool> Update(User user)
+        {
+            var result = await _httpClient.PutAsJsonAsync<User>("User/Update-User", user);
+            if (result.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var result = await _httpClient.DeleteAsync($"User/Delete-User?id={id}");
+            if (result.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
