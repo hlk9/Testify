60502fdf02207d0c39fd7b1e1f8b93185649e542
SNAKE01112004 <nuyendoanquangluc1112004@gmail.com>
2024-09-10 15:43:14 +0700

    commit repo

--- Testify.DAL/Reposiroties/UserPermissionRepository.cs
+++ Testify.DAL/Reposiroties/UserPermissionRepository.cs
@@ -11,6 +11,18 @@ namespace Testify.DAL.Reposiroties
 {
     public class UserPermissionRepository
     {
-            
+        private readonly TestifyDbContext _context;
+        public UserPermissionRepository()
+        {
+            _context = new TestifyDbContext();
+        }
+        public async Task<List<UserPermission>> GetAllUserPermissions()
+        {
+            return await _context.UserPermissions.ToListAsync();
+        }
+        public async Task<UserPermission> GetByidUserPermission(int id)
+        {
+            return await _context.UserPermissions.FindAsync(id);
+        }
     }
 }
