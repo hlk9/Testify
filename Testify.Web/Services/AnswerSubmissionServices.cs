214b8337f2692d83b4b4063ec833b960b822f897
Bacdtx <bacdtx.atwork@gmail.com>
2024-10-08 17:06:14 +0700

    them crud submission va answersubmission

---
+++ Testify.Web/Services/AnswerSubmissionServices.cs
@@ -0,0 +1,22 @@
+﻿using Testify.DAL.Models;
+
+namespace Testify.Web.Services
+{
+    public class AnswerSubmissionServices
+    {
+        HttpClient _httpClient;
+
+        public AnswerSubmissionServices(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<AnswerSubmission> CreateAnswerSubmission(AnswerSubmission answerSubmission)
+        {
+            var obj = await _httpClient.PostAsJsonAsync("AnswerSubmission/Create-AnswerSubmission", answerSubmission);
+            var reponse = await obj.Content.ReadFromJsonAsync<AnswerSubmission>();
+
+            return reponse;
+        }
+    }
+}
