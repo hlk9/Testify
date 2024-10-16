abe4783e653cabedc07deca394e92db5f3b4f55c
Hoang Long <hoanglong.cmb@gmail.com>
2024-10-16 11:55:31 +0700

    add

---
+++ Testify.API/Controllers/ClassExamScheduleController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Testify.DAL.Reposiroties;
+using Testify.DAL.ViewModels;
+
+namespace Testify.API.Controllers
+{
+    [Route("ClassExamSchedule")]
+    [ApiController]
+    public class ClassExamScheduleController : ControllerBase
+    {
+
+        ClassExamScheduleRepository repos;
+        public ClassExamScheduleController()
+        {
+            repos = new ClassExamScheduleRepository();
+        }
+
+        [HttpGet("Get-Class-ByScheduleId")]
+        public List<ClassWithUser> GetClassByScheduleId(int  scheduleId)
+        {
+            return repos.GetClassInSchedule(scheduleId);
+        }
+               
+    }
+}
