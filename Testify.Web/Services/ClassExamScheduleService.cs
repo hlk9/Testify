43d3128f0637e647fa1b388c7ad38b653b084300
Hoang Long <hoanglong.cmb@gmail.com>
2024-10-16 11:57:12 +0700

    add class exam schedule

---
+++ Testify.Web/Services/ClassExamScheduleService.cs
@@ -0,0 +1,21 @@
+﻿using Testify.DAL.Models;
+using Testify.DAL.ViewModels;
+
+namespace Testify.Web.Services
+{
+    public class ClassExamScheduleService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ClassExamScheduleService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public  async Task<List<ClassWithUser>> GetClassInSchedule(int scheduleId)
+        {
+            return await _httpClient.GetFromJsonAsync<List<ClassWithUser>>("ClassExamSchedule/Get-Class-ByScheduleId?scheduleId=" +scheduleId);
+        }
+    }
+    
+}
