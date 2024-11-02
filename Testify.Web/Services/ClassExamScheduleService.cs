9742eb791caed08fa8897b982861f87aecb190e3
Hoang Long <hoanglong.cmb@gmail.com>
2024-11-02 23:38:46 +0700

    add method to get StudentExist

--- Testify.Web/Services/ClassExamScheduleService.cs
+++ Testify.Web/Services/ClassExamScheduleService.cs
@@ -1,4 +1,5 @@
-﻿using Testify.API.DTOs;
+﻿using Azure;
+using Testify.API.DTOs;
 using Testify.DAL.Models;
 using Testify.DAL.ViewModels;
 
@@ -13,15 +14,16 @@ namespace Testify.Web.Services
             _httpClient = httpClient;
         }
 
-        public  async Task<List<ClassWithUser>> GetClassInSchedule(int scheduleId)
+        public async Task<List<ClassWithUser>> GetClassInSchedule(int scheduleId)
         {
-            return await _httpClient.GetFromJsonAsync<List<ClassWithUser>>("ClassExamSchedule/Get-Class-ByScheduleId?scheduleId=" +scheduleId);
+            return await _httpClient.GetFromJsonAsync<List<ClassWithUser>>("ClassExamSchedule/Get-Class-ByScheduleId?scheduleId=" + scheduleId);
         }
 
         public async Task<bool> AddListClassToSchedule(List<ClassWithUser> data, int scheduleId)
         {
-            var a = await _httpClient.PostAsJsonAsync("ClassExamSchedule/Add-ListClassToSchedule?scheduleId="+scheduleId,data);
-            if(a.IsSuccessStatusCode)
+            var lis = CheckListClassInSchedule(data, scheduleId);
+            var a = await _httpClient.PostAsJsonAsync("ClassExamSchedule/Add-ListClassToSchedule?scheduleId=" + scheduleId, data);
+            if (a.IsSuccessStatusCode)
                 return true;
             return false;
         }
@@ -34,15 +36,34 @@ namespace Testify.Web.Services
             return false;
         }
 
-        public async Task<List<ClassWithUser>> CheckListClassInSchedule(List<ClassWithUser> data, int scheduleId)
+        public async Task<List<User>> CheckListClassInSchedule(List<ClassWithUser> data, int scheduleId)
         {
             CheckClassScheduleRequest reqData = new CheckClassScheduleRequest();
             reqData.DataList = data;
-            reqData.ScheduleId = scheduleId;    
-            var a = await _httpClient.PostAsJsonAsync<CheckClassScheduleRequest>("ClassExamSchedule/Get-StudentExist-InSchedule",reqData);
-          
+            reqData.ScheduleId = scheduleId;
+            var a = await _httpClient.PostAsJsonAsync<CheckClassScheduleRequest>("ClassExamSchedule/Checks-StudentExist-InSchedule", reqData);
+
+            if (a.IsSuccessStatusCode)
+            {
+                if (a.Content != null)
+                {
+                    try
+                    {
+                        var response = await a.Content.ReadFromJsonAsync<List<User>>();
+
+                        return response;
+                    }
+                    catch (Exception e) {
+                    }
+
+                   
+
+                }
+            }
+            return null;
+
         }
 
     }
-    
+
 }
