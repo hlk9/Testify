dfc317ffd0c4af591685c6e683586c0df68b4704
Hoang Long <hoanglong.cmb@gmail.com>
2024-11-12 16:16:03 +0700

    add log service

---
+++ Testify.Web/Data/Commons/LogService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+public class LogService
+{
+    private readonly ILogger<LogService> _logger;
+
+    public LogService(ILogger<LogService> logger)
+    {
+        _logger = logger;
+    }
+
+    public void LogInformation(string message)
+    {
+        _logger.LogInformation(message);
+    }
+
+    public void LogError(string message, Exception exception)
+    {
+        _logger.LogError(exception, message);
+    }
+
+    public void LogWarning(string message)
+    {
+        _logger.LogWarning(message);
+    }
+}
