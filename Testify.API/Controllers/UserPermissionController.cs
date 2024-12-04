4a83b5b8a758c061316d3b839a61d4656b49c6c9
Hoang Long <hoanglong.cmb@gmail.com>
2024-12-04 09:00:12 +0700

    update permission user

--- Testify.API/Controllers/UserPermissionController.cs
+++ Testify.API/Controllers/UserPermissionController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Testify.DAL.Models;
+using Testify.DAL.Reposiroties;
 
 namespace Testify.API.Controllers
 {
@@ -6,6 +8,37 @@ namespace Testify.API.Controllers
     [ApiController]
     public class UserPermissionController : ControllerBase
     {
+        UserPermissionRepository userPermissionRepository;
 
+        public UserPermissionController()
+        {
+            userPermissionRepository = new UserPermissionRepository();
+        }
+
+        [HttpPost("Create-UserPermission")]
+        public async Task<bool> Create(UserPermission userPermission)
+        {
+            var obj =  userPermissionRepository.AddUserPermission(userPermission);
+            return obj;
+        }
+
+        [HttpDelete("Delete-UserPermission")]
+        public async Task<bool> Delete(int id)
+        {
+            var obj =  userPermissionRepository.DeleteUserPermission(id);
+            return obj;
+        }
+
+        [HttpGet("Get-PermissionByUserId")]
+        public async Task<List<UserPermission>> GetPermissionByUserId(Guid id)
+        {
+            return await userPermissionRepository.GetByUserIdPermission(id);
+        }
+
+        [HttpGet("Check-PermissionByUserIdAndName")]
+        public async Task<bool> CheckPermission(Guid userId, string permission)
+        {
+            return await userPermissionRepository.HasPermissionAsync(userId, permission);
+        }
     }
 }
