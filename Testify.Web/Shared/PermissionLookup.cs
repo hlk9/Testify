3c80ec131ad54be46f65b05c01dcdef09dc13030
Hoang Long <hoanglong.cmb@gmail.com>
2024-12-04 09:01:39 +0700

    add lookup

---
+++ Testify.Web/Shared/PermissionLookup.cs
@@ -0,0 +1,10 @@
+﻿namespace Testify.Web.Shared
+{
+    public static class PermissionLookup
+    {
+        public const string ManageExam = "ManageExam";
+        public const string ManageQuestion = "ManageQuestion";
+        public const string ManageAnswer = "ManageAnswer";
+        public const string ManageClass = "ManageClass";
+    }
+}
